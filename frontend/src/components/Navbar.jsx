d7098e649babe2e2c3dfa84caf6bbc9ba381ac87
pavan-234 <varanasipavankalyan07@gmail.com>
2025-03-28 22:27:32 +0530

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import DarkModeToggle from "./DarkModeToggle";
 
 const Navbar = () => {
   return (
+    //classname
     <nav className="p-4 flex justify-between bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
       <h1 className="text-xl font-bold">MemeForge</h1>
       <DarkModeToggle />
