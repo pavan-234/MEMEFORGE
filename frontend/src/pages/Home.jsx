7936b01ce82b7b7e69ce8fe2d3c25ea2acf65cca
pavan-234 <varanasipavankalyan07@gmail.com>
2025-03-28 22:28:24 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MemeCard from "../components/MemeCard";
 
 const Home = () => {
   return (
+    //home
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Trending Memes</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
